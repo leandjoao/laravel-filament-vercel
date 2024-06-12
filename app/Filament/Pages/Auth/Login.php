a70894d3f69c41b6711edcdb892d4eb2b32678e8
Joao Leandro <leand.joao@outlook.com.br>
2024-06-12 12:55:17 +0100

    Fresh Start

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Pages\Auth\Login as BasePage;
+
+class Login extends BasePage
+{
+    public function mount(): void
+    {
+        parent::mount();
+        $this->form->fill([
+            'email' => 'admin@scheduler.com',
+            'password' => 'secret',
+            'remember' => true,
+        ]);
+    }
+}
